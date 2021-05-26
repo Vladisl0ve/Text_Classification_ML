f28509efbe24ecba15b892bb80dd31e7f3346e8b
Vladislove <vladislav.lapko@gmail.com>
2021-05-26 18:52:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Text_Classification_ML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TextML.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Classification_ML
+{
+    class TextML
+    {
+        string[] _text;
+        Dictionary<string, int> InfoWordCounter = new Dictionary<string, int>();
+        public TextML(string[] text)
+        {
+
+        }
+    }
+}
